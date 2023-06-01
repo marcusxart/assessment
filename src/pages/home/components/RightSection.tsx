292595860865f228cff49a8b7935148f917f72d2
marcusxart <cemeji64@gmail.com>
2023-05-31 17:51:55 -0700

    update

--- src/pages/home/components/RightSection.tsx
+++ src/pages/home/components/RightSection.tsx
@@ -3,7 +3,7 @@ import SuggestedUsersOrPages from "./SuggestedUsersOrPages";
 
 const RightSection = () => {
   return (
-    <aside className="w-[411px] pt-[100px]">
+    <aside className="w-[329px] pt-[100px]">
       <p className="text-xl text-strongPurple mb-[21px]">Suggested Listings</p>
       <SuggestedList />
       <SuggestedUsersOrPages />
--- src/pages/home/components/SuggestedUsersOrPages.tsx
+++ src/pages/home/components/SuggestedUsersOrPages.tsx
@@ -1,7 +1,40 @@
 import { useState } from "react";
+import Button from "../../../components/Button";
 
 const Item = () => {
-  return <div></div>;
+  return (
+    <div className="flex w-full items-center gap-[13px] ">
+      <img
+        src="./img/ellipse-32-1@1x.png"
+        alt=""
+        className="w-[47px] h-[47px]"
+      />
+      <div className="flex flex-1 items-center gap-[18px]">
+        <div className="flex items-center flex-1 justify-between">
+          <div>
+            <p className="text-[15px] text-strongPurple font-semibold">
+              Rose Kooto
+            </p>
+            <p className="text-textGrey text-xs">Abuja</p>
+          </div>
+          <Button
+            text="Support"
+            style={{
+              fontSize: "12px",
+              width: "78px",
+              height: "27px",
+              borderRadius: "13px",
+            }}
+          />
+        </div>
+        <img
+          src="./img/icon-feather-bookmark-1@1x.png"
+          className="w-[11px] h-[14px]"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 const SuggestedUsersOrPages = () => {
@@ -28,7 +61,12 @@ const SuggestedUsersOrPages = () => {
           Suggested Pages
         </p>
       </div>
-      <div className="w-full flex flex-col pt-[23px] px-[25px] pb-[34px] bg-lightPurple rounded-2xl">
+      <div className="w-full flex flex-col pt-[23px] px-[25px] pb-[34px] bg-lightPurple rounded-2xl gap-5">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
         <Item />
       </div>
     </div>
